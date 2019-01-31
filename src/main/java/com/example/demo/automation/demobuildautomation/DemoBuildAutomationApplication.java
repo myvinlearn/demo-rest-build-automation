e73a87e91f65fe02fb390843038b332b3cfcdc48
Vinod Tummuri <vtummuri@arrow.com>
2019-01-31 15:50:50 -0700

    initial commit

---
+++ src/main/java/com/example/demo/automation/demobuildautomation/DemoBuildAutomationApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo.automation.demobuildautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBuildAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBuildAutomationApplication.class, args);
+	}
+
+}
+
